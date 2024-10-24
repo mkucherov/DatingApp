a5021f6312a37f6f9dd1af43860eb41070fdfcfe
Michael Kucherov <mkucherov@outlook.com>
2024-10-24 07:57:37 -0500

    Committing changes before Angular update

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,8 @@ builder.Logging.AddConsole();
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Add the databse context
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -17,10 +19,26 @@ builder.Services.AddDbContext<DataContext>(opt =>
 );
 
 
-var app = builder.Build();
+//var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+//app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader()
+              .AllowAnyMethod()
+              .WithOrigins("http://localhost:4200", "https://localhost:4200");
+    });
+});
+
+var app = builder.Build();
+
+// Use CORS with the specified policy
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
